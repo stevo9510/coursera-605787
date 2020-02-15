a6858bea006cb032bb1cf575459bd20f069d3f89
stevo9510 <stevo9510@gmail.com>
2020-02-15 01:41:29 -0500

    All optional requirements added for module 6

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 
 [Module 5 Solution](http://stevo9510.github.io/coursera-605787/module5-solution "Module 5 Solution")
 
+[Module 6 Solution](http://stevo9510.github.io/coursera-605787/module6-solution "Module 6 Solution")
--- module6-solution/app.js
+++ module6-solution/app.js
@@ -7,13 +7,11 @@
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope){
         $scope.items = "";
-        $scope.message = "";
-        $scope.emptyItemMessage = "";
 
         $scope.checkIfTooMuch = function(){
-            if($scope.items === ""){
-                $scope.message = "Please enter data first";
-                $scope.emptyItemMessage = "";
+            
+            if($scope.items == undefined || $scope.items === ""){
+                invalidInput();
             }
             else {
                 var itemsArr = $scope.items.split(',');
@@ -22,15 +20,24 @@
                 });
 
                 $scope.emptyItemMessage = itemsArr.length != filteredArr.length 
-                    ? "Empty items not counted toward count." : "";
+                    ? "Note: Empty items not counted toward count." : "";
 
-                if(filteredArr.length <= 3){
-                    $scope.message = "Enjoy!";
+                /* This is a special case we're handling where the user entered
+                    all commas, so item length was not 0.  Not specified how to
+                    handle this in requirements, so I made something up that made sense.
+                    (It did not make sense to say 'Enjoy' with 0 items :) ) 
+                */
+                if(filteredArr.length == 0){
+                    allEmptyItems();
+                }
+                else if(filteredArr.length <= 3){
+                    validInput("Enjoy!");
                 } 
                 else {
-                    $scope.message = "Too much!";
+                    validInput("Too much!");
                 }
 
+                // Just for debugging/logging.
                 console.log("Current Items: " + $scope.items);
                 console.log("Non-Filtered Array");
                 console.log(itemsArr);
@@ -40,5 +47,24 @@
                 console.log("Filtered Count: " + filteredArr.length);
             }
         };
+
+        function invalidInput(){
+            $scope.message = "Please enter data first";
+            $scope.emptyItemMessage = "";
+            $scope.messageStyle = "invalid-msg-text";
+            $scope.inputStyle = "invalid-msg-input";
+        }
+
+        function validInput(msg){
+            $scope.message = msg;
+            $scope.messageStyle = "valid-msg-text";
+            $scope.inputStyle = "valid-msg-input";
+        }
+
+        function allEmptyItems(){
+            $scope.message = "All empty items were entered. Please enter non-empty items";
+            $scope.messageStyle = "";
+            $scope.inputStyle = "";
+        }
     }
 })();

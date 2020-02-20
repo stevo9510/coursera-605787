b7bcb4c6c0e67cf93b55b3cee5d590b2828c95bd
stevo9510 <stevo9510@gmail.com>
2020-02-19 22:24:01 -0500

    All extra requirements for assignment 7 implemented

--- module7-solution/app.js
+++ module7-solution/app.js
@@ -4,17 +4,25 @@
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .filter('angularMoney', AngularMoneyFilter)
         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService){
-
         var tbList = this;
 
         tbList.items = ShoppingListCheckOffService.getToBuyItems();
 
         tbList.buyItem = function(itemIndex) {
             ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+
+        tbList.isNaturalNumber = function(val) {
+            /* Note: Didn't use Number.isInteger as that's not supported in IE11 */
+            return !isNaN(val) 
+                && val != '' // NaN will return true for empty strings
+                && Math.floor(val) == val  // ensure it's a whole number.
+                && val >= 0;  // and non-negative
         };
     }
 
@@ -50,7 +58,17 @@
         service.getBoughtItems = function () {
             return boughtItems;
         };
+    }
 
+    // Custom Filter.  Note that a lot of this could be handled using built in
+    // currency filter, however, decided to implement it all here to make it more interesting.
+    // See note in HTML of a way to handle it with Currency filter.
+    function AngularMoneyFilter() {
+        return function (input) {
+            input = input || 0;
+            input = "$$$" + input.toFixed(2);
+            return input;     
+        }
     }
 
 })();

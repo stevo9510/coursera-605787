45dde526776ce377ee9219bd29343cd0f891430a
stevo9510 <stevo9510@gmail.com>
2020-05-02 15:13:15 -0400

    Leave field invalidated when value changes, until re-verified when focus is lost.

--- module10-solution/src/public/sign-up/sign-up.controller.js
+++ module10-solution/src/public/sign-up/sign-up.controller.js
@@ -9,6 +9,12 @@
         var $ctrl = this;
         $ctrl.user = {};
 
+        // automatically devalidate field when the value changes, because we'll
+        // want to revalidate it when it loses focus.  
+        $ctrl.menuItemChanged = function () {
+            $scope.regForm.menunumber.$setValidity("invalid", false);
+        };
+
         $ctrl.menuItemOnBlur = function () {
             var promise = MenuService.getMenuItemPromise($ctrl.user.menunumber);
 
@@ -22,19 +28,8 @@
         }
 
         $ctrl.submit = function () {
-            var promise = MenuService.getMenuItemPromise($ctrl.user.menunumber);
-
-            promise.then(function (response) {
-                console.log("Menu Item Response: ", response.data);
-                MyInfoService.setUserInfo($ctrl.user);
-                $scope.regForm.menunumber.$setValidity("invalid", true);
-                $ctrl.successfullySaved = true;
-
-            })
-                .catch(function (error) {
-                    $scope.regForm.menunumber.$setValidity("invalid", false);
-                    $ctrl.successfullySaved = false;
-                });
+            MyInfoService.setUserInfo($ctrl.user);
+            $ctrl.successfullySaved = true;
         };
     };
 

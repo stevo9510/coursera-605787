8649fbf728278d5d11310cee0ae431e95ddbccef
stevo9510 <stevo9510@gmail.com>
2020-05-02 14:46:45 -0400

    Show additional my info fields.

--- module10-solution/src/public/my-info/my-info.controller.js
+++ module10-solution/src/public/my-info/my-info.controller.js
@@ -10,12 +10,9 @@
         $ctrl.favoriteMenuItem = null;
 
         $ctrl.userInfo = MyInfoService.getUserInfo();
-        console.log("User Info:", $ctrl.userInfo);
         if ($ctrl.userInfo && $ctrl.userInfo.menunumber) {
-            console.log("Loaded Preference: ", $ctrl.userInfo.menunumber);
             MenuService.getMenuItemPromise($ctrl.userInfo.menunumber)
                 .then(function (response) {
-                    console.log("Response Data: ", response.data);
                     $ctrl.favoriteMenuItem = response.data;
                 });
         }
